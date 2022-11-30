f96cc6f051e3e059e341d2b1c41ea7a540a89e8e
jayden <90698041+jjaydope@users.noreply.github.com>
2022-11-30 17:21:11 +0700

    data base

---
+++ heartless.sql
@@ -0,0 +1,369 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Nov 2022 pada 11.20
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `heartless`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bagian`
+--
+
+CREATE TABLE `bagian` (
+  `id_bagian` int(11) NOT NULL,
+  `nama_bagian` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `bagian`
+--
+
+INSERT INTO `bagian` (`id_bagian`, `nama_bagian`) VALUES
+(1, 'sekertariat'),
+(2, 'kepegawaian');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `kode_barang` char(20) NOT NULL,
+  `nama_barang` varchar(256) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `kode_barang`, `nama_barang`, `stok`) VALUES
+(1, '172839173917', 'tranqulity base hotel n casion', 14),
+(2, 'B0050', 'baterai amplas', 7),
+(3, 'HJ19', 'hungry jack pancake', 9),
+(4, 'B0051', 'Harry House', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_masuk` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_masuk`, `id_barang`, `tgl_masuk`, `stok`) VALUES
+(1, 1, '2022-09-26', 5);
+
+--
+-- Trigger `barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `delete` AFTER DELETE ON `barang_masuk` FOR EACH ROW BEGIN 
+UPDATE barang SET stok = stok-old.stok 
+WHERE id_barang = old.id_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `masuk` AFTER INSERT ON `barang_masuk` FOR EACH ROW BEGIN
+UPDATE barang set stok= stok + NEW.stok WHERE id_barang = NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `tgl` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail`, `id_transaksi`, `id_barang`, `qty`, `tgl`) VALUES
+(1, 1, 1, 1, '2022-09-26'),
+(2, 1, 2, 2, '2022-09-26'),
+(3, 2, 3, 1, '2022-09-26'),
+(4, 2, 4, 1, '2022-09-26'),
+(5, 3, 2, 1, '2022-11-30');
+
+--
+-- Trigger `detail_transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus` AFTER DELETE ON `detail_transaksi` FOR EACH ROW BEGIN
+UPDATE barang SET stok= stok +old.qty 
+WHERE id_barang = old.id_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `keluar` AFTER INSERT ON `detail_transaksi` FOR EACH ROW BEGIN
+UPDATE barang set stok= stok - NEW.qty WHERE id_barang = NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `disbarang`
+--
+
+CREATE TABLE `disbarang` (
+  `id` int(1) NOT NULL,
+  `barang_id` int(1) NOT NULL,
+  `qty` int(100) NOT NULL,
+  `potongan` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member`
+--
+
+CREATE TABLE `member` (
+  `NIP` int(11) NOT NULL,
+  `nama` varchar(256) NOT NULL,
+  `id_bagian` int(11) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `member`
+--
+
+INSERT INTO `member` (`NIP`, `nama`, `id_bagian`, `password`) VALUES
+(52116140, 'jaad malik', 1, '6c34fd5b51dcdd56ad9204c67209d6b5'),
+(52116147, 'jaden hossler', 2, 'aeb9f2b1a53aaced2f5007f7246d7090'),
+(52116176, 'juned', 2, '4769ad580a03eae7501b5852ea17b82a');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `nama_petugas` varchar(256) NOT NULL,
+  `role` char(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `role`) VALUES
+(1, 'yawnzzn', '2c6d36260e6a76f1073c81bec5fa7a6d', 'yeonjun choi', 'admin'),
+(2, 'alll', '01f0ef6792b64e990d788dc9612eaa32', 'Alex david T', 'admin'),
+(3, 'y0ungblood', '61b4ffccd6a65c59251377628505d1c6', 'Luke Hemmings', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `NIP` int(11) NOT NULL,
+  `tgl` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_petugas`, `NIP`, `tgl`) VALUES
+(1, 1, 52116140, '2022-09-26'),
+(2, 1, 52116176, '2022-09-26'),
+(3, 1, 52116147, '2022-11-30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `bagian`
+--
+ALTER TABLE `bagian`
+  ADD PRIMARY KEY (`id_bagian`),
+  ADD KEY `id_bagian` (`id_bagian`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_masuk`),
+  ADD KEY `id_masuk` (`id_masuk`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_detail` (`id_detail`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `disbarang`
+--
+ALTER TABLE `disbarang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`NIP`),
+  ADD KEY `NIP` (`NIP`),
+  ADD KEY `id_bagian` (`id_bagian`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`),
+  ADD KEY `id_petugas` (`id_petugas`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `NIP` (`NIP`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `bagian`
+--
+ALTER TABLE `bagian`
+  MODIFY `id_bagian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `disbarang`
+--
+ALTER TABLE `disbarang`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD CONSTRAINT `barang_masuk_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel `member`
+--
+ALTER TABLE `member`
+  ADD CONSTRAINT `member_ibfk_1` FOREIGN KEY (`id_bagian`) REFERENCES `bagian` (`id_bagian`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`NIP`) REFERENCES `member` (`NIP`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
